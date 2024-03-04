449383eafd75244deda1db24bbd183842ae410e9
Shaddy <shadybett540@gmail.com>
2024-03-04 22:23:32 +0300

    successfully set up flowbite and tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // Include JavaScript and TypeScript files
-    "./public/index.html", // Include your HTML file
+    "./src/**/*.{js,jsx,ts,tsx}",
+    'node_modules/flowbite-react/lib/esm/**/*.js'
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('flowbite/plugin')],
 };
