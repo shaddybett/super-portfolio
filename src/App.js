436dfcf17800d67cdfe8e6e3574e850868b5d10a
Shaddy <shadybett540@gmail.com>
2024-03-08 23:33:19 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from 'react'
 import Home from './components/Home'
 import { Route,Routes } from 'react-router-dom'
-import Projects from './components/Projects'
-import Contact from './components/Contact'
-import Skills from './components/Skills'
-import About from './components/About'
+
+
 
 
 function App() {
@@ -12,10 +10,6 @@ function App() {
     <div>
       <Routes>
         <Route path='/' element={<Home/>} />
-        <Route path='/projects' element={<Projects/>}/>
-        <Route path='/contact' element={<Contact/>}/>
-        <Route path='/skills' element={<Skills/>}/>
-        <Route path='/about' element={<About/>}/>
 
       </Routes>
     </div>
